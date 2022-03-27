0f5dd2ba871832c87bef38d8bc3c460142d1f378
hunko <rhwlgns4386@naver.com>
2022-03-28 08:36:24 +0900

    Update FacealbumApplication.java

--- src/main/java/com/kkanggogo/facealbum/FacealbumApplication.java
+++ src/main/java/com/kkanggogo/facealbum/FacealbumApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FacealbumApplication {
+public class FaceAlbumApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FacealbumApplication.class, args);
+        SpringApplication.run(FaceAlbumApplication.class, args);
     }
 
 }
