b84df8955dbe0f9231ae6a17e2382760065426e6
hunko <rhwlgns4386@naver.com>
2022-03-28 08:35:39 +0900

    project first comit

---
+++ src/main/java/com/kkanggogo/facealbum/FacealbumApplication.java
@@ -0,0 +1,13 @@
+package com.kkanggogo.facealbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacealbumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacealbumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kkanggogo/facealbum/FacealbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkanggogo.facealbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacealbumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
