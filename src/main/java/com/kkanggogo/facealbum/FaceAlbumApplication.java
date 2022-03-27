8b100c5de920676d260e738754a4a097621e9c41
hunko <rhwlgns4386@naver.com>
2022-03-28 08:40:33 +0900

    Create FaceAlbumApplication.java

---
+++ src/main/java/com/kkanggogo/facealbum/FaceAlbumApplication.java
@@ -0,0 +1,13 @@
+package com.kkanggogo.facealbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceAlbumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaceAlbumApplication.class, args);
+    }
+
+}
