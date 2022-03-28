e0b3a5637bd86b46d09a865f8d6fc1c3d2d9664d
hunko <rhwlgns4386@naver.com>
2022-03-28 17:15:07 +0900

    s3업로드 완료

--- src/main/java/com/kkanggogo/facealbum/S3ImageSave.java
+++ src/main/java/com/kkanggogo/facealbum/S3ImageSave.java
@@ -1,19 +1,34 @@
 package com.kkanggogo.facealbum;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class S3ImageSave implements SaveImage {
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
 
+    public final S3Uploader s3Uploader;
 
     @Override
     public void save(Object file) {
-      log.debug("S3bucket={}",bucket);
+        UUID uuid = UUID.randomUUID();
+        String path="static/"+uuid+".jpg";
+        String upload;
+        if(file instanceof MultipartFile){
+            upload=s3Uploader.upload(((MultipartFile) file), path);
+        }
+        else {
+            upload=s3Uploader.upload((byte[]) file,path);
+        }
+
+        log.info(upload);
     }
 }
