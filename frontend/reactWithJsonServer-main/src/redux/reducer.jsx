ffc514b2cd622e610d173ac0541f955b8c9ed466
caffeinejun <gurwns232e@gmail.com>
2022-03-30 17:36:00 +0900

    frontend image CRUD

---
+++ frontend/reactWithJsonServer-main/src/redux/reducer.jsx
@@ -0,0 +1,46 @@
+import * as types from './actionType'
+
+const initialState = {
+    users: [],
+    user: {},
+    loading: true,
+}
+
+const usersReducers = (state = initialState, action) => {
+    switch (action.type) {
+        case types.GET_USERS:
+            return {
+                ...state,
+                users: action.payload,
+                loading: false,
+            }
+
+        case types.DELETE_USER:
+            return {
+                ...state,
+                loading: false,
+            }
+
+        case types.ADD_USER:
+            return {
+                ...state,
+                loading: false,
+            }
+
+        case types.GET_SINGLE_USER:
+            return {
+                ...state,
+                user: action.payload,
+                loading: false,
+            }
+        case types.UPDATE_USER:
+            return {
+                ...state,
+                loading: false,
+            }
+        default:
+            return state
+    }
+}
+
+export default usersReducers
